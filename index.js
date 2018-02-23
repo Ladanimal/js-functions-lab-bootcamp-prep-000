bdb59dfd3b27bf272639919aaddbe998a140ef2b
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-23 00:32:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-function happyHolidays(string)
-{
-  return console.log('Happy Holidays!'))
-}
 
-function happyHolidaysTo(string) 
-{
- console.log("Happy ${holidays}, ${name}!") 
-}
-
-function holidayCountdown(string)
-{
-  console.log("it's ${days} days until ${holiday}!")
-}
