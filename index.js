8c1a4b968764806855882e998e19a9c576c92154
ladanziaee@gmail.com <ladanziaee@gmail.com>
2018-02-23 00:31:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// write your code below!
+function happyHolidays(string)
+{
+  return console.log('Happy Holidays!'))
+}
+
+function happyHolidaysTo(string) 
+{
+ console.log("Happy ${holidays}, ${name}!") 
+}
+
+function holidayCountdown(string)
+{
+  console.log("it's ${days} days until ${holiday}!")
+}
